aca42f65137701b4d121f79efd413451ff294d43
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-07-06 16:19:32 -0300

    add api port variable

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import  cors from 'cors'
 import bodyParser from "body-parser";
 import {routes} from "./routes/routes";
+import {apiPort} from "./static/variables";
 const server = express()
 server.use(bodyParser.json())
 server.use(cors())
 server.use(routes)
 
-server.listen(3333,()=>{
+server.listen(apiPort,()=>{
     console.log("server iniciado!")
 })
--- src/static/variables.ts
+++ src/static/variables.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const jwtSecretKey = process.env.JWTKEY as string
 export const databaseSecretKet = process.env.DATABASE_KEY as string
+export  const apiPort = process.env.API_PORT as string
